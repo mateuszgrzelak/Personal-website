2c4ea80840cae51a0237826be96ab43a8515160f
Mategrze <mgrzelak1997@wp.pl>
2020-07-03 11:20:45 +0200

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 const routes: Routes = [
   { path: 'aboutme', component: AboutMeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
 
   { path: '**', redirectTo: 'aboutme', pathMatch: 'full' },
 ];
