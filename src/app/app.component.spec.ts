324d7e2ae82c1e48bb5d2628d2098c87c39e3517
Mateusz Grzelak <mgrzelak1997@wp.pl>
2021-07-27 22:29:29 +0200

    deployment testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('#greeting').textContent).toContain('Hello there!!!');
+    expect(compiled.querySelector('#greeting').textContent).toContain('Deployment testing');
   });
 });
